c12fa88b5d234ed55bd6406a905101ff92a2a362
Dhilip Buddha <dhilipb@gmail.com>
2017-12-31 12:52:55 +0000

    Adding multiple users

--- index.js
+++ index.js
@@ -11,36 +11,46 @@ const request = require('request');
 
 
 app.get('/', (req, res) => {
-    var user = 'alanmasters'
-    request('https://www.tradingview.com/u/' + user, function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
+    var users = (req.query.users || 'alanmasters').split(',')
+    
+    var doneRequests = 0;
+    
+    for (let userIndex in users) {
+        var user = users[userIndex]
+        console.log("Running for " + user)
+        request('https://www.tradingview.com/u/' + user, function (error, response, html) {
+            if (!error && response.statusCode == 200) {
+                const $ = cheerio.load(html);
 
-            $(".js-feed-item .js-widget-idea").each(function(i, element) {
-                const title = $(this).find(".tv-widget-idea__title-name").text().trim()
-                const description = $(this).find(".tv-widget-idea__description-text").text().trim()
-                const url = response.request.uri.href + $(this).find(".tv-widget-idea__title").attr("href")
-                const author = $(this).find(".tv-user-link__name").text().trim()
-                const date = parseInt($(this).find(".tv-widget-idea__time").attr("data-timestamp"), 10)
-                const feedItem = {
-                    title: title,
-                    description: description,
-                    url: url,
-                    author: author,
-                    date: date
-                }
+                $(".js-feed-item .js-widget-idea").each(function(i, element) {
+                    const title = $(this).find(".tv-widget-idea__title-name").text().trim()
+                    const description = $(this).find(".tv-widget-idea__description-text").text().trim()
+                    const url = response.request.uri.href + $(this).find(".tv-widget-idea__title").attr("href")
+                    const author = $(this).find(".tv-user-link__name").text().trim()
+                    const date = parseInt($(this).find(".tv-widget-idea__time").attr("data-timestamp"), 10)
+                    const feedItem = {
+                        title: title,
+                        description: description,
+                        url: url,
+                        author: author,
+                        date: date
+                    }
 
-                console.log(feedItem)
-                feed.item(feedItem)
-            })
+                    // console.log(feedItem)
+                    feed.item(feedItem)
+                })
 
-            res.set('Content-Type', 'application/rss+xml');
-            res.send(feed.xml({indent: true}))
-        } else {
-            res.send()
-        }
-    });
-});
+            }
+
+            doneRequests++;
+            if (doneRequests == users.length) {
+                console.log("Done")
+                res.set('Content-Type', 'text/xml');
+                res.send(feed.xml({indent: true}))
+            }
+        });
+    }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
